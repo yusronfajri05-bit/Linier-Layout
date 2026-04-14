dae8298e03651285bb16ffb1abae45752e3839a0
Yusron Fajri <yusronfajri05@gmail.com>
2026-04-15 05:13:12 +0700

    first commit

---
+++ app/src/main/java/com/example/linearlayout/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.linearlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.linearlayout.ui.theme.LinearLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.linearlayoutapp)
+
+
+    }
+}
